052c7b6d5f084c9e19126da8c0dc3ff9ce8f55ab
Ryan Canuel <ryancanuel@hotmail.com>
2021-03-02 20:44:08 -0500

    Added PlatformerFollowCamera, fixed unresponsive jumping, added double jump

--- Assets/Scripts/Platformer2DPlayerController.cs
+++ Assets/Scripts/Platformer2DPlayerController.cs
@@ -6,8 +6,13 @@ public class Platformer2DPlayerController : PhysicsObject {
 
     public float jumpTakeOffSpeed = 5f;
     public float maxSpeed = 7f;
+    public float jumpMinTime = .25f;
     public bool groundJump = true;
     public bool midAirJump = true;
+
+    [SerializeField] 
+    private float timeSinceJump = 0f;
+    private bool jumpReleased = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,8 @@ public class Platformer2DPlayerController : PhysicsObject {
 
         Vector3 move = Vector3.zero;
 
+        timeSinceJump += Time.deltaTime;
+
         move.x = Input.GetAxis("Horizontal");
 
         if(grounded) {
@@ -28,8 +35,10 @@ public class Platformer2DPlayerController : PhysicsObject {
             groundJump = false;
         }
 
-        if(Input.GetButtonDown("Jump")) {
+        if(Input.GetButtonDown("Jump") || Input.GetKeyDown("w")) {
             if(groundJump || midAirJump) {
+                timeSinceJump = 0f;
+                jumpReleased = false;
                 velocity.y = jumpTakeOffSpeed;
                 if(groundJump) {
                     groundJump = false;
@@ -37,7 +46,14 @@ public class Platformer2DPlayerController : PhysicsObject {
                     midAirJump = false;
                 }
             }
-        } else if(Input.GetButtonUp("Jump")) {
+        }
+        
+        if(Input.GetButtonUp("Jump") || Input.GetKeyUp("w")) {
+            jumpReleased = true;
+        }
+        
+        if(timeSinceJump >= jumpMinTime && jumpReleased) {
+            jumpReleased = false;
             if(velocity.y > 0f) {
                 velocity.y *= .5f;
             }
---
+++ Assets/Scripts/PlatformerFollowCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerFollowCamera : MonoBehaviour
+{
+
+    public Transform followObject;
+    public float smoothSpeed = .01f;
+
+    private Vector3 smoothPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = new Vector3(followObject.position.x, followObject.position.y, transform.position.z);
+        smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+        transform.position = smoothPosition;
+    }
+}
