11353155453718498f572b687ce405cc7f865b8c
Ryan Canuel <ryancanuel@hotmail.com>
2021-03-04 03:07:44 -0500

    Implemented throwable javelins
    jesus

--- Assets/Scripts/Javelin.cs
+++ Assets/Scripts/Javelin.cs
@@ -3,18 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Javelin : PhysicsObject
-{
-    private bool frozen;
+{   
+    public float projectileSpeed;
+    public float rotationSpeed;
+    public GameObject player;
+    public float minAge;
+
+    [SerializeField]
+    private Vector3 throwPosition;
+    private bool frozen = false;
+    private float age;
 
     // Start is called before the first frame update
     void Start() {
         frozen = false;
+
+        age = 0f;
+
+        throwPosition = transform.position;
+
+        // read in mouse position so we can fire this quiil at it
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Find the delta from the launchPos to the mousePos3D
+        Vector3 launchDirection = -throwPosition - -mousePos3D;
+        launchDirection.Normalize();
+
+        transform.position = throwPosition + launchDirection;
+
+        launchDirection *= projectileSpeed;
+
+        velocity.y = launchDirection.y;
+        velocity.x = launchDirection.x;
+        targetVelocity = launchDirection;
+
+        //Debug.Log("launchDirection is " + launchDirection);
+        //Debug.Log("Projectile's velocity is " + velocity);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics.gravity * Time.deltaTime;
+        velocity.x = targetVelocity.x;
+
+        age += Time.deltaTime;
+
+        grounded = false;
+
+        Vector3 positionChange = velocity * Time.deltaTime;
+
+        //Vector3 moveAlongGround = new Vector3(groundNormal.y, -groundNormal.x, 0f);
+
+        Vector3 move = new Vector3(1f, 0f, 0f) * positionChange.x;
+
+        Movement(move, false);
+
+        move = Vector3.up * positionChange.y;
+
+        Movement(move, true);
+
+        //targetVelocity = Vector3.zero;
+        //ComputeVelocity();
+        //Debug.Log("Projectile's velocity is " + velocity);
     }
 
     public void destroyJavelin() {
@@ -28,9 +86,16 @@ public class Javelin : PhysicsObject
 
         float distance = move.magnitude;
 
-        if(frozen) {
-            distance = 0;
+        if(frozen && age >= minAge) {
+            velocity.y = 0;
+            return;
         }
+
+        // quill rotation through air
+        //transform.rotation = Quaternion.Slerp(transform.rotation,
+        //                                    Quaternion.LookRotation(velocity),
+        //                                    Time.deltaTime * rotationSpeed);
+        transform.LookAt(transform.position + velocity);
 
         if (distance > minMoveDistance) {
 
@@ -41,9 +106,11 @@ public class Javelin : PhysicsObject
             // or another javelin. also, we need a way to notify 
             // PlayerQuillThrower to remove it from the javelinList
 
-            if (collide) {
+            if (collide && hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Ground")) {
 
                 frozen = true;
+
+                //gameObject.layer = LayerMask.NameToLayer("ground");
 
                 //float projection = Vector3.Dot(velocity, currentNormal);
 
@@ -61,5 +128,11 @@ public class Javelin : PhysicsObject
 
             rb.position = rb.position + move.normalized * distance;
         }
+    }
+
+    public void SetVelocity(Vector3 vel) {
+        velocity = vel;
+        targetVelocity = vel;
+        //Debug.Log("Velocity & tV set to " + velocity);
     }
 }
--- Assets/Scripts/PlayerQuillThrower.cs
+++ Assets/Scripts/PlayerQuillThrower.cs
@@ -6,11 +6,16 @@ public class PlayerQuillThrower : MonoBehaviour
 {
     public GameObject projectile;
     public float throwInterval = .2f;
-    public float projectileSpeed = 20f;
+    //public float projectileSpeed = 20f;
     public GameObject player;
+    public Vector3 test;
 
+    //[SerializeField]
     private float timeSinceThrow = 0f;
-    private List<Javelin> javelinList = new List<Javelin>();
+    //[SerializeField]
+    private List<GameObject> javelinList = new List<GameObject>();
+    //[SerializeField]
+    private Vector3 throwPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -22,30 +27,62 @@ public class PlayerQuillThrower : MonoBehaviour
     // Update is called once per frame
     void Update() {
         timeSinceThrow += Time.deltaTime;
+        throwPosition = player.transform.position + new Vector3(0f, .5f, 0f);
 
-        if(Input.GetButtonDown("Fire")) {
+        if (Input.GetButtonDown("Fire1")) {
+            //Debug.Log("Fire1 clicked");
             ThrowQuill();
         }
     }
 
     private void ThrowQuill() {
+
+        if(timeSinceThrow < throwInterval) {
+            return;
+        }
+
         int count = javelinList.Count;
         // ensure that there are no more than 3 quills on screen at once-
         // if there are 3 or more, remove until there are 2, so new quill becomes third
         if(count >= 3) {
-            for(int i = count; i <= 3; i--) {
-                Javelin toDie = javelinList[0];
+            for(int i = count; i >= 3; i--) {
+                GameObject toDie = javelinList[0];
                 javelinList.RemoveAt(0);
-                toDie.destroyJavelin();
+                Destroy(toDie);
             }
         }
+        //Debug.Log("count is " + count);
 
         // instantiate and set up quill
         GameObject go = Instantiate<GameObject>(projectile);
+        javelinList.Add(go);
         go.layer = LayerMask.NameToLayer("PlayerProjectile");
-        go.transform.position = player.transform.position + new Vector3(0f, 1.5f, 0f);
+        go.transform.position = throwPosition;
         timeSinceThrow = 0f;
 
+        // screw it, let the javelin throw itself on instantiation
 
+        //// read in mouse position so we can fire quiils at it
+        //Vector3 mousePos2D = Input.mousePosition;
+        //mousePos2D.z = -Camera.main.transform.position.z;
+        //Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        
+        //// Find the delta from the launchPos to the mousePos3D
+        //Vector3 launchDirection =  -throwPosition - -mousePos3D;
+        //launchDirection.Normalize();
+
+        //Javelin javelinScript = projectile.GetComponent<Javelin>();
+        //if(javelinList != null) {
+        //    javelinScript.SetVelocity(launchDirection * projectileSpeed);
+        //}
+
+        //Debug.Log("Projectile's initial velocity is " + javelinScript.)
+
+        //if(projectile is Javelin jav) {
+        //    jav.velocity = launchDirection.Normalized();
+        //} else {
+        //    Debug.Log("Tried to set projectile's velocity, projectile is not a javelin");
+        //}
+        
     }
 }
